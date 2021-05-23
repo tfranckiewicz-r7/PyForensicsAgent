b91b3aa61091a9f8904d52d5cdc88b294cedaeca
tfranckiewicz <tomasz_franckiewicz@rapid7.com>
2021-05-23 11:59:01 +0200

    Updated semantic_version to

--- agent/__init__.py
+++ agent/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -
 import agent.common as ac
 
-__version__ = 1621568114
-__semantic_version__ = '3.0.10.1'
+__version__ = 1621353659
+__semantic_version__ = '3.0.9.7'
 
 ac.set_agent_env()
